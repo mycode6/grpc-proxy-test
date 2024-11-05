a20cb457eb27ff7897512668df7d0f8966207627
bh-go <254894102@qq.com>
2024-11-05 17:58:25 +0800

    两种情况需改进

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
+use std::{env, path::PathBuf};
+
 use failure::Error;
 
 fn main() -> Result<(), Error> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
-        .file_descriptor_set_path("bin")
+        .file_descriptor_set_path(out_dir.join("echo.bin"))
         .compile(
             &["proto/echo.proto"],
             &["googleapis", "proto"],
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use tonic::{transport::Server, Request, Response, Status};
 
 use crate::api::echo_service_server::{EchoService, EchoServiceServer};
-use api::Message;
+use api::{HelloRequest, Message};
 
 pub mod api {
     tonic::include_proto!("api");
+    pub const ECHO_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("echo");
 }
 
 #[derive(Debug, Default)]
@@ -14,11 +16,28 @@ pub struct EchoAPI {}
 impl EchoService for EchoAPI {
     async fn echo(
         &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<Message>, Status> {
+        let r = request.into_inner();
+        let value = r.message.unwrap().value;
+        println!("received {:?}", value);
+        if value == "test" {
+            return Err(Status::invalid_argument("value参数不允许使用test"));
+        }
+
+        Ok(Response::new(Message { value: "response".into() }))
+    }
+
+    async fn echo1(
+        &self,
         request: Request<Message>,
     ) -> Result<Response<Message>, Status> {
         let message = request.into_inner();
-
-        println!("received {:?}", message.value);
+        let value = message.value;
+        println!("received {:?}", value);
+        if value == "test" {
+            return Err(Status::invalid_argument("value参数不允许使用test"));
+        }
 
         Ok(Response::new(Message { value: "response".into() }))
     }
@@ -33,7 +52,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // let sss = tonic_reflection::server::Builder::configure()
     let sss = tonic_reflect_protobuf::server::Builder::configure()
-        .register_encoded_file_descriptor_set(include_bytes!("/Users/codeman/Documents/Github/mycode6/grpc-proxy-test/bin"))
+        .register_encoded_file_descriptor_set(api::ECHO_DESCRIPTOR_SET)
         .build().unwrap();
 
     Server::builder()
